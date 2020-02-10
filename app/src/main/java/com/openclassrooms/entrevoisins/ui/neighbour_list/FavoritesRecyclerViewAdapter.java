eba9422f58f95bbae2199d34d39ea5361ccd5156
Sansho <sansho88@hotmail.fr>
2020-02-10 17:41:53 +0100

    26/01/2019 11h30
    Tests unitaires au vert.
    Polissage

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/FavoritesRecyclerViewAdapter.java
@@ -0,0 +1,70 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class FavoritesRecyclerViewAdapter extends RecyclerView.Adapter<FavoritesRecyclerViewAdapter.ViewHolder> {
+
+    protected final List<Neighbour> listeFavoris;
+
+    public FavoritesRecyclerViewAdapter(List<Neighbour> items) {
+
+        listeFavoris = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_neighbour, parent, false);
+        System.out.println("[DEBUG: FavRecycView] Liste FAV créée");
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        Neighbour favNeighbour = listeFavoris.get(position);
+        holder.mNeighbourName.setText(favNeighbour.getName());
+        Glide.with(holder.mNeighbourAvatar.getContext())
+                .load(favNeighbour.getAvatarUrl())
+                .apply(RequestOptions.circleCropTransform())
+                .into(holder.mNeighbourAvatar);
+
+
+    }
+    @Override
+    public int getItemCount() {
+        System.out.println("[DEBUG]: Taille liste favoris: " + listeFavoris.size());
+        return listeFavoris.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.item_list_avatar)
+        public ImageView mNeighbourAvatar;
+        @BindView(R.id.item_list_name)
+        public TextView mNeighbourName;
+        @BindView(R.id.item_list_delete_button)
+        public ImageButton mDeleteButton;
+
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
